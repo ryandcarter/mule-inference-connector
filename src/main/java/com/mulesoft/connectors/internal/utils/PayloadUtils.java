3b2b9f26308c26608ec58ccb0bcb56920eccbac5
Amir Khan <amir.khan@salesforce.com>
2025-04-16 22:07:13 +0200

    o4-mini-support
    o4-mini-support

--- src/main/java/com/mulesoft/connectors/internal/utils/PayloadUtils.java
+++ src/main/java/com/mulesoft/connectors/internal/utils/PayloadUtils.java
@@ -17,7 +17,7 @@ import java.util.Base64;
  * Utility class for payload operations.
  */
 public class PayloadUtils {
-    private static final String[] NO_TEMPERATURE_MODELS = {"o3-mini", "o1", "o1-mini"};
+    private static final String[] NO_TEMPERATURE_MODELS = {"o3-mini","o3","o4-mini","o4", "o1", "o1-mini"};
 
     /**
      * Build the payload for the API request
