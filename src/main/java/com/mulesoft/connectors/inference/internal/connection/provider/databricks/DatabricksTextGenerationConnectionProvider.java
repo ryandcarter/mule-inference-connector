56c491e340be9e0db7743fa4fe90949b76e26671
ayush-salesforce <162944854+ayush-salesforce@users.noreply.github.com>
2025-06-29 21:52:06 +0530

    Feature/w 18897961 add databricks and negative tests (#174)
    * Add negative tests
    
    * Changes for databricks. Model is not directly used but instead use service endpoint
    
    * Add integration for deepseek, it might not work in local but should work in pipeline
    
    * Add integration for perplexity for text operations only
    
    * Enable vision tests for groq
    
    * Add model provider test for perplexity
    
    * Add max tokens for perplexity

--- src/main/java/com/mulesoft/connectors/inference/internal/connection/provider/databricks/DatabricksTextGenerationConnectionProvider.java
+++ src/main/java/com/mulesoft/connectors/inference/internal/connection/provider/databricks/DatabricksTextGenerationConnectionProvider.java
@@ -4,18 +4,16 @@ import org.mule.runtime.api.connection.ConnectionException;
 import org.mule.runtime.api.meta.ExpressionSupport;
 import org.mule.runtime.extension.api.annotation.Alias;
 import org.mule.runtime.extension.api.annotation.Expression;
-import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
 import org.mule.runtime.extension.api.annotation.param.ParameterGroup;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
+import org.mule.runtime.extension.api.annotation.param.display.Example;
 import org.mule.runtime.extension.api.annotation.param.display.Placement;
-import org.mule.runtime.extension.api.annotation.values.OfValues;
 
 import com.mulesoft.connectors.inference.internal.connection.parameters.TextGenerationConnectionParameters;
 import com.mulesoft.connectors.inference.internal.connection.provider.TextGenerationConnectionProvider;
 import com.mulesoft.connectors.inference.internal.connection.types.databricks.DatabricksTextGenerationConnection;
 import com.mulesoft.connectors.inference.internal.dto.ParametersDTO;
-import com.mulesoft.connectors.inference.internal.llmmodels.databricks.providers.DatabricksTextGenerationModelNameProvider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,14 +27,16 @@ public class DatabricksTextGenerationConnectionProvider extends TextGenerationCo
   @Parameter
   @Placement(order = 1)
   @Expression(ExpressionSupport.SUPPORTED)
-  @OfValues(DatabricksTextGenerationModelNameProvider.class)
-  private String databricksModelName;
+  @Example(value = "databricks-meta-llama-3-3-70b-instruct")
+  @DisplayName("[Databricks] Serving Endpoint Name")
+  private String servingEndpointName;
 
   @Parameter
+  @Placement(order = 2)
   @Expression(ExpressionSupport.SUPPORTED)
-  @Optional(defaultValue = "https://<instance>.cloud.databricks.com/")
-  @DisplayName("[Databricks] Model URL")
-  private String dataBricksModelUrl;
+  @Example(value = "https://<instance>.cloud.databricks.com/")
+  @DisplayName("[Databricks] Serving Endpoint URL host")
+  private String servingEndpointUrlHost;
 
   @ParameterGroup(name = Placement.CONNECTION_TAB)
   private TextGenerationConnectionParameters textGenerationConnectionParameters;
@@ -45,13 +45,13 @@ public class DatabricksTextGenerationConnectionProvider extends TextGenerationCo
   public DatabricksTextGenerationConnection connect() throws ConnectionException {
     logger.debug("DatabricksTextGenerationConnection connect ...");
     return new DatabricksTextGenerationConnection(getHttpClient(), getObjectMapper(),
-                                                  new ParametersDTO(databricksModelName,
+                                                  new ParametersDTO(servingEndpointName,
                                                                     textGenerationConnectionParameters.getApiKey(),
                                                                     textGenerationConnectionParameters.getMaxTokens(),
                                                                     textGenerationConnectionParameters.getTemperature(),
                                                                     textGenerationConnectionParameters.getTopP(),
                                                                     textGenerationConnectionParameters.getTimeout()),
-                                                  dataBricksModelUrl,
+                                                  servingEndpointUrlHost,
                                                   textGenerationConnectionParameters.getMcpSseServers());
   }
 }
